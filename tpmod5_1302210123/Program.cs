8eb544c7b3e092aa387f01054cb00f0fd8be0af8
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-03-19 23:11:32 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmod5_1302210123/Program.cs
+++ tpmod5_1302210123/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class HaloGeneric
+{
+    public void GreetUser<T>(T user)
+    {
+        Console.WriteLine($"Hello user {user}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        string user = "Muhammad Rafa Eka Pramoedya";
+        halo.GreetUser(user);
+    }
+}
